6fe16d5112f32c8afc77ba1d6076bf7c657d47b6
AE <drew@Andrews-MacBook-Pro.local>
2023-08-30 10:58:07 -0600

    first and last commit, tutorial completed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+
+export default function App() {
+  return (
+      <div className="wrapper">
+          <header>
+            <img className="background" src="../Mountain.png"/>
+            <img className="foreground" src="../Trees.png"/>
+            <h1 className="title">Welcome!</h1>
+          </header>
+          <section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In mollis nunc sed id semper risus in. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Praesent semper feugiat nibh sed pulvinar. Iaculis at erat pellentesque adipiscing. Volutpat lacus laoreet non curabitur. Egestas diam in arcu cursus euismod quis viverra nibh. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. At in tellus integer feugiat scelerisque varius. Velit sed ullamcorper morbi tincidunt ornare massa eget. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Tristique nulla aliquet enim tortor at auctor urna. Quis vel eros donec ac odio tempor orci dapibus. Magna fermentum iaculis eu non diam phasellus vestibulum lorem.
+
+            Nullam non nisi est sit amet. Est placerat in egestas erat imperdiet sed euismod nisi. Id interdum velit laoreet id donec ultrices tincidunt arcu non. Aliquam ut porttitor leo a. Mauris nunc congue nisi vitae suscipit tellus mauris. Eu facilisis sed odio morbi quis commodo odio aenean. Massa tincidunt dui ut ornare lectus sit. Risus ultricies tristique nulla aliquet enim tortor at auctor. Augue interdum velit euismod in pellentesque massa. Faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus mauris ultrices eros in cursus turpis massa. Nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi. Convallis posuere morbi leo urna molestie at elementum eu facilisis. Quis imperdiet massa tincidunt nunc pulvinar sapien. Posuere morbi leo urna molestie at elementum eu. Scelerisque eu ultrices vitae auctor. Mauris augue neque gravida in fermentum et.
+
+            Aenean sed adipiscing diam donec adipiscing tristique. Aliquet porttitor lacus luctus accumsan tortor. Blandit libero volutpat sed cras ornare arcu. Id diam maecenas ultricies mi eget. Lectus magna fringilla urna porttitor. Mattis pellentesque id nibh tortor. Sodales ut eu sem integer vitae. Cursus in hac habitasse platea. Aliquet nibh praesent tristique magna sit amet purus gravida quis. Sed faucibus turpis in eu. Porttitor eget dolor morbi non arcu risus quis varius quam. Lectus arcu bibendum at varius. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Quam nulla porttitor massa id neque aliquam vestibulum. Eu facilisis sed odio morbi. Commodo odio aenean sed adipiscing diam donec adipiscing tristique.
+
+            At lectus urna duis convallis convallis tellus. Ultrices tincidunt arcu non sodales neque. Tincidunt eget nullam non nisi est sit amet facilisis. Amet facilisis magna etiam tempor orci eu. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est. Adipiscing commodo elit at imperdiet dui accumsan sit. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. In cursus turpis massa tincidunt dui. Tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius. Sagittis eu volutpat odio facilisis mauris sit amet massa. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Justo laoreet sit amet cursus sit amet dictum sit amet. Morbi leo urna molestie at elementum. Facilisis magna etiam tempor orci eu. Nunc scelerisque viverra mauris in aliquam. Vulputate mi sit amet mauris commodo. Odio tempor orci dapibus ultrices in iaculis nunc sed.
+              
+              Aenean sed adipiscing diam donec adipiscing tristique. Aliquet porttitor lacus luctus accumsan tortor. Blandit libero volutpat sed cras ornare arcu. Id diam maecenas ultricies mi eget. Lectus magna fringilla urna porttitor. Mattis pellentesque id nibh tortor. Sodales ut eu sem integer vitae. Cursus in hac habitasse platea. Aliquet nibh praesent tristique magna sit amet purus gravida quis. Sed faucibus turpis in eu. Porttitor eget dolor morbi non arcu risus quis varius quam. Lectus arcu bibendum at varius. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Quam nulla porttitor massa id neque aliquam vestibulum. Eu facilisis sed odio morbi. Commodo odio aenean sed adipiscing diam donec adipiscing tristique.
+
+            At lectus urna duis convallis convallis tellus. Ultrices tincidunt arcu non sodales neque. Tincidunt eget nullam non nisi est sit amet facilisis. Amet facilisis magna etiam tempor orci eu. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est. Adipiscing commodo elit at imperdiet dui accumsan sit. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. In cursus turpis massa tincidunt dui. Tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius. Sagittis eu volutpat odio facilisis mauris sit amet massa. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Justo laoreet sit amet cursus sit amet dictum sit amet. Morbi leo urna molestie at elementum. Facilisis magna etiam tempor orci eu. Nunc scelerisque viverra mauris in aliquam. Vulputate mi sit amet mauris commodo. Odio tempor orci dapibus ultrices in iaculis nunc sed.
+          </section>
+      </div>
+  )
+}
+
